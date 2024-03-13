9d9e13cec1d5100f8c92db8649817c96bf3128aa
Nahuel <nahue_cge@hotmail.com>
2024-03-13 15:50:35 -0300

    createmodels

--- apps/servicios/models.py
+++ apps/servicios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=240)
+    descripcion = models.CharField(max_length=240)
+
+class Pack(models.Model):
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=240)
+    descripcion = models.CharField(max_length=240)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    precio = models.DecimalField()
